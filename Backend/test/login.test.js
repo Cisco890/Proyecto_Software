0164c0270ed166aa66a95303cbabadf72f6d5e82
0liRem <olivierviaus@gmail.com>
2025-08-05 14:33:40 -0600

    Encrypted tests 2

--- Backend/test/login.test.js
+++ Backend/test/login.test.js
@@ -6,6 +6,13 @@ const crypto = require("crypto");
 
 const prisma = new PrismaClient();
 
+const algorithm = "aes-256-cbc";
+const key = crypto.scryptSync(
+  process.env.SECRET_KEY || "clave_super_secreta",
+  "salt",
+  32
+);
+
 const randomSuffix = Date.now();
 const TEST_USER = {
   nombre: "Usuario Test",
@@ -20,6 +27,14 @@ function encrypt(text) {
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return iv.toString("hex") + ":" + encrypted;
+}
+function decrypt(encrypted) {
+  const [ivHex, encryptedText] = encrypted.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encryptedText, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
 }
 
 beforeAll(async () => {
